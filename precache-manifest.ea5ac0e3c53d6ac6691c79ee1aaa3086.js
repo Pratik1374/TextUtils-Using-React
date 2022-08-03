10c8284e966a03e9421965ba2d6efecf343efe01
Pratik <pratik1374@gmail.com>
2022-08-03 22:20:09 +0530

    Updates

---
+++ precache-manifest.ea5ac0e3c53d6ac6691c79ee1aaa3086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e4e7a51eea0e8a3ec1",
+    "url": "/TextUtils-Using-React/static/css/main.6ac1c0d4.chunk.css"
+  },
+  {
+    "revision": "e3e4e7a51eea0e8a3ec1",
+    "url": "/TextUtils-Using-React/static/js/main.e3e4e7a5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-Using-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-Using-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "49a2ef4d729ab4994395",
+    "url": "/TextUtils-Using-React/static/js/runtime~main.49a2ef4d.js"
+  },
+  {
+    "revision": "bcf61faef9102ef206a747ce0d7ec255",
+    "url": "/TextUtils-Using-React/index.html"
+  }
+];
